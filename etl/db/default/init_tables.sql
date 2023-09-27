b30eb89c1a03c0241fd29df18b38a2162b928e30
Frédéric BIDON <fredbi@yahoo.com>
2023-09-27 14:49:02 +0200

    first stab at deploying a postgres db
    Signed-off-by: Frédéric BIDON <fredbi@yahoo.com>

---
+++ etl/db/default/init_tables.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA data
+
+CREATE TABLE data.entity_product(
+);
+CREATE TABLE data.taxo_product_identifier(
+);
+CREATE TABLE data.taxo_product_enum1(
+);
+CREATE TABLE data.taxo_product_enum2(
+);
+CREATE TABLE data.taxo_item_enum1(
+);
+CREATE TABLE data.taxo_item_enum2(
+);
+CREATE TABLE data.taxo_item_identifier(
+);
+CREATE TABLE data.taxo_mapping(
+);
+
+CREATE TABLE data.product_history(
+);
+CREATE TABLE data.product_has_identifier(
+);
+CREATE TABLE data.entity_item(
+);
+CREATE TABLE data.item_history(
+);
+CREATE TABLE data.item_has_identifier(
+);
+CREATE TABLE data.product_has_item(
+);
